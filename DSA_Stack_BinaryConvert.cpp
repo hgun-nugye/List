fd292e70d87deb56a77c4b5fc1b98606afafae63
hgun-nugye <huong.ntt.65cntt@ntu.edu.vn>
2024-10-13 08:41:45 +0700

    correctBinary

--- DSA_Stack_BinaryConvert.cpp
+++ DSA_Stack_BinaryConvert.cpp
@@ -1,60 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+struct nut {
     int giatri;
-    struct Node* next;
+    struct nut *next;
 };
-typedef struct Node Node;
+typedef struct nut Node;
 
 struct Stack {
-    Node* top;
+    Node *top;
 };
 typedef struct Stack StackType;
 
-StackType *Binary(StackType **stack, int n) 
+//prototype
+StackType *PushBinary(StackType **stack, int n);
+void ViewBinary(StackType *stack);
+
+int main() 
 {
-    StackType* temp = (StackType*)malloc(sizeof(StackType));
-    temp->top = NULL;
-
-    while (n != 1) {
-        Node* newNode = (Node*)malloc(sizeof(Node));
-        newNode->giatri = n % 2;
-        newNode->next = temp->top;
-        temp->top = newNode;
-        n /= 2;
-    }
-
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->giatri = n;
-    newNode->next = temp->top;
-    temp->top = newNode;
-
-    return temp;
-}
-
-void View(StackType* stack) {
-    while (stack->top != NULL) {
-        printf("%d", stack->top->giatri);
-        stack->top = stack->top->next;
-    }
-}
-
-int main() {
-    StackType* stack;
+    StackType *stack;
     int n;
-
+		printf("Nhap vao so nguyen he 10 can chuyen doi: ");
     scanf("%d", &n);
-    stack = Binary(&stack, n);
-    View(stack);
+    stack = PushBinary(&stack, n);
+    printf("He 2 cua so vua nhap la:  ");
+    ViewBinary(stack);
 
     // Free the stack
-    while (stack->top != NULL) {
-        Node* temp = stack->top;
+    while (stack->top != NULL) 
+		{
+        Node *temp = stack->top;
         stack->top = stack->top->next;
         free(temp);
     }
     free(stack);
-
     return 0;
+}
+
+//function
+StackType *PushBinary(StackType **stack, int n) 
+{
+    (*stack)->top = NULL;
+
+    while (n!= 1) 
+		{
+        Node *newNode = (Node*)malloc(sizeof(Node));
+        newNode->giatri=n%2;
+        newNode->next=(*stack)->top;
+        (*stack)->top=newNode;
+        n/=2;
+    }
+
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->giatri = n;
+    newNode->next =(*stack)->top;
+    (*stack)->top = newNode;
+
+    return *stack;
+}
+void ViewBinary(StackType *stack) 
+{
+    while (stack->top != NULL) 
+		{
+        printf("%d", stack->top->giatri);
+        stack->top = stack->top->next;
+    }
 }
